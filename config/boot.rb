d1cb15067f0bbc382df3b4f2b12e3a92fa213265
Andrii Kuluiev <andreykuluev@gmail.com>
2026-05-06 02:26:09 +0300

    Fix docker compose

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'dotenv/load'   # Load .env before anything else reads ENV
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
