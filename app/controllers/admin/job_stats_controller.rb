7ce14252c2efcb4955d86895bd8ae59c280c07e3
Andrii Kuluiev <andreykuluev@gmail.com>
2026-05-23 06:47:15 +0300

    Revert "Add job charts"
    This reverts commit 75b36b78dcccbf1910e0a8c38cd9b91c34abacca.

--- app/controllers/admin/job_stats_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Admin::JobStatsController < Admin::BaseController
-  def index
-    endpoint Admin::JobStats::Operation::Index, Admin::JobStats::Component::Index
-  end
-end
