2bf82476ef1011491806a1d3045f373c05caf5d2
Andrii Kuluiev <andreykuluev@gmail.com>
2026-05-25 03:56:37 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-Oaken.seed :users
+require 'benchmark'
+
+time = Benchmark.realtime do
+  Oaken.loader.seed :users
+  Oaken.loader.seed :proxies
+  Oaken.loader.seed :sources
+  Oaken.loader.seed :vacancies
+end
+
+puts "Time to create seeds: #{time.round(3)} seconds" # rubocop:disable Rails/Output
